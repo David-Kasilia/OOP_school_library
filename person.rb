fbb06f93a048873d007f2b439dd999e089403568
David-Kasilia <davidkasilia001@gmail.com>
2022-10-31 16:31:07 +0300

    Create teacher class and fix linters

--- person.rb
+++ person.rb
@@ -1,22 +1,21 @@
 class Person
-    attr_reader :id
-    attr_accessor :name, :age
+  attr_reader :id
+  attr_accessor :name, :age
 
-    def initialize(age, name = 'Unknown', parent_permission: true)
-        @id = Random.rand(1..10_000)
-        @name = name
-        @age = age
-        @parent_permission = parent_permission
-    end
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = Random.rand(1..10_000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
 
-    def can_use_services?
-        @parent_permission || is_of_age?
-    end
+  def can_use_services?
+    @parent_permission || is_of_age?
+  end
 
-    private :of_age?
+  private :of_age?
 
-    def of_age?
-        @age >= 18
-    end
-    
+  def of_age?
+    @age >= 18
+  end
 end
