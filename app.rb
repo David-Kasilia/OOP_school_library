ba21add4c6697bef3d6fa0305e734bdaaaabb69a
David-Kasilia <davidkasilia001@gmail.com>
2022-11-03 15:03:29 +0300

    Create list all people method

--- app.rb
+++ app.rb
@@ -17,3 +17,10 @@ class App
             puts "(#{index + 1}) Book => Title: #{book.title}, Author: #{book.author}"
         end
     end
+    # List all people method
+    def list_all_people
+        puts 'There are no people in the list. Kindly add at least one person' if @people.empty?
+        @people.each_with_index do |person, index|
+            puts "(#{index + 1}) [#{person.class}] => Id: #{person.id}, Name: #{person.name}, Age: #{person.age}"
+        end
+    end
