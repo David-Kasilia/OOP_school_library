4453fe2d54cf04c2aafd63da0d2b67c1b8b34121
David-Kasilia <davidkasilia001@gmail.com>
2022-11-03 15:08:33 +0300

    Create list of all rentals books

--- app.rb
+++ app.rb
@@ -17,6 +17,7 @@ class App
             puts "(#{index + 1}) Book => Title: #{book.title}, Author: #{book.author}"
         end
     end
+    
     # List all people method
     def list_all_people
         puts 'There are no people in the list. Kindly add at least one person' if @people.empty?
@@ -24,6 +25,7 @@ class App
             puts "(#{index + 1}) [#{person.class}] => Id: #{person.id}, Name: #{person.name}, Age: #{person.age}"
         end
     end
+
     # Create a person method (teacher or student not a plain person)
     def create_a_person()
         puts 'Do you want to create a student(1) or a teacher(2)? [Enter a number 1 or 2]: ' 
@@ -47,6 +49,7 @@ class App
             puts "#{name.capitalize} was added as a teacher successfully"
         end
     end
+
       # Create a book
       def create_a_book
         puts 'Create a new book'
@@ -57,6 +60,7 @@ class App
         @books.push(Book.new(title, author))
         puts "#{title} book was added successfully"
     end
+
    # Create a rental 
    def create_a_rental()
     puts 'Select a book from the following list by the book number: '
@@ -72,4 +76,19 @@ class App
     puts 'Date of renting book is added'
     @rentals.push(Rental.new(date, @people[person_number - 1], @books[book_number - 1]))
     puts 'Rental book created successfully'
-    end    
+    end
+
+    # List all rentals for a given person id
+    def list_all_rentals
+        print 'Enter Person\'s ID: '
+        id = gets.chomp.to_i
+        puts 'List of all Rentals books: '
+        @rentals.each do |rental|
+            if rental.person.id == Id
+                puts "Person: #{rental.person.name} Date: #{rental.date}, Book '#{rental.book.title}' written by #{rental.book.author}"
+            else
+                puts 'Checking for person......'
+                puts 'No rentals found for the given ID for person'
+            end
+        end
+    end
