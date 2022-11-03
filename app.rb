b1cc6a3ae60014bdb68aa023458da782b7d11169
David-Kasilia <davidkasilia001@gmail.com>
2022-11-03 15:05:07 +0300

    Create person method

--- app.rb
+++ app.rb
@@ -24,3 +24,26 @@ class App
             puts "(#{index + 1}) [#{person.class}] => Id: #{person.id}, Name: #{person.name}, Age: #{person.age}"
         end
     end
+    # Create a person method (teacher or student not a plain person)
+    def create_a_person()
+        puts 'Do you want to create a student(1) or a teacher(2)? [Enter a number 1 or 2]: ' 
+        cartegory = gets.chomp.to_i
+        case cartegory
+        when 1
+            print 'Enter the student name: '
+            name = gets.chomp
+            print 'Enter the student\'s age: '
+            age = gets.chomp
+            @people.push(Student.new('classroom', name, age))
+            puts "#{name.capitalize} was added as a student successfully"
+            when 2
+            print 'Enter the teacher name: '
+            name = gets.chomp
+            print 'Enter the teacher\'s age: '
+            age = gets.chomp
+            print 'Enter the teacher\'s specialization: '
+            specialization = gets.chomp
+            @people.push(Teacher.new(name, age, specialization))
+            puts "#{name.capitalize} was added as a teacher successfully"
+        end
+    end
