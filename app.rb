8a0bb6d63d8dbb14bba4db573bebfa3c0e59af50
David-Kasilia <davidkasilia001@gmail.com>
2022-11-04 16:55:00 +0300

    Introduced a bug

--- app.rb
+++ app.rb
@@ -13,7 +13,7 @@ class App
   # List all books method
   def list_all_books
     puts 'There are no books found, Kindly add at least one book' if @books.empty?
-    @books.each_with_index do |book, index|
+    @books.each_book_index do |book, index|
       puts "(#{index + 1}) Book => Title: #{book.title}, Author: #{book.author}"
     end
   end
@@ -21,7 +21,7 @@ class App
   # List all people method
   def list_all_people
     puts 'There are no people in the list. Kindly add at least one person' if @people.empty?
-    @people.each_with_index do |person, index|
+    @people.each_person_index do |person, index|
       puts "(#{index + 1}) [#{person.class}] => Id: #{person.id}, Name: #{person.name}, Age: #{person.age}"
     end
   end
