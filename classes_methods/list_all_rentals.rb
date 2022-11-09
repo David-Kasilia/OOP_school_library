22b3d316ac7e0e4f1d464dd4a5bae756fae3030e
David-Kasilia <davidkasilia001@gmail.com>
2022-11-09 12:49:10 +0300

    Prepare classes for preserve data

--- classes_methods/list_all_rentals.rb
+++ classes_methods/list_all_rentals.rb
@@ -6,8 +6,6 @@ class ListAllRentals
     selected_person_id = GetUserInput.get_user_input_int('Enter Person \'s ID: ')
     puts 'Here Is A List Of All The Rentals'
     @rentals = app.list_rentals_for_given_id(selected_person_id)
-    @rentals.each do |rental|
-      puts "Date: #{rental.date}, Book '#{rental.book.title}' written by #{rental.book.author}"
-    end
+    @rentals.each {  |rental| puts "Date: #{rental.date}, Book '#{rental.book.title}' written by #{rental.book.author}" }
   end
 end
