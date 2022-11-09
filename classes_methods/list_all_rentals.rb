ff7b26ffa352cf336f912f804a534d97b1af88d7
David-Kasilia <davidkasilia001@gmail.com>
2022-11-09 13:21:08 +0300

    Testing and fixing rubocop linters

--- classes_methods/list_all_rentals.rb
+++ classes_methods/list_all_rentals.rb
@@ -6,6 +6,8 @@ class ListAllRentals
     selected_person_id = GetUserInput.get_user_input_int('Enter Person \'s ID: ')
     puts 'Here Is A List Of All The Rentals'
     @rentals = app.list_rentals_for_given_id(selected_person_id)
-    @rentals.each {  |rental| puts "Date: #{rental.date}, Book '#{rental.book.title}' written by #{rental.book.author}" }
+    @rentals.each do |rental|
+      puts "Date: #{rental.date}, Book '#{rental.book.title}' written by #{rental.book.author}"
+    end
   end
 end
